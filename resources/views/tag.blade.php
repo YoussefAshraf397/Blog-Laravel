755c94d2c972e7f8f76fa58c66d4c3ef83842751
JoeAshraf <youssefashraf397@yahoo.com>
2020-08-23 02:25:49 +0200

    Get post by category and tags

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -1,50 +1,27 @@
 @extends('layouts.frontend.app')
 
-@section('title' , 'BLOG')
+@section('title','Tag')
 
 @push('css')
-    <link href="{{asset('assets/frontend/css/home/styles.css')}}" rel="stylesheet">
-
-    <link href="{{asset('assets/frontend/css/home/responsive.css')}}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/category/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/frontend/css/category/responsive.css') }}" rel="stylesheet">
+    <style>
+        .favorite_posts{
+            color: blue;
+        }
+    </style>
 @endpush
 
 @section('content')
-
-    <div class="main-slider">
-        <div class="swiper-container position-static" data-slide-effect="slide" data-autoheight="false"
-             data-swiper-speed="500" data-swiper-autoplay="10000" data-swiper-margin="0" data-swiper-slides-per-view="4"
-             data-swiper-breakpoints="true" data-swiper-loop="true" >
-            <div class="swiper-wrapper">
-                @foreach($categories as $category)
-                    <div class="swiper-slide">
-                        <a class="slider-category" href="{{ route('category.posts',$category->slug) }}">
-                            <div class="blog-image"><img src="{{ Storage::disk('public')->url('category/slider/'.$category->image) }}" alt="{{ $category->name }}"></div>
-
-                            <div class="category">
-                                <div class="display-table center-text">
-                                    <div class="display-table-cell">
-                                        <h3><b>{{$category->name}}</b></h3>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </a>
-                    </div><!-- swiper-slide -->
-
-                @endforeach
-
-
-            </div><!-- swiper-wrapper -->
-
-        </div><!-- swiper-container -->
-
+    <div class="slider display-table center-text">
+        <h1 class="title display-table-cell"><b>{{ $tag->name }}</b></h1>
     </div><!-- slider -->
 
     <section class="blog-area section">
         <div class="container">
 
             <div class="row">
-                @foreach($posts as $post)
+                @forelse($posts as $post)
                     <div class="col-lg-4 col-md-6">
                         <div class="card h-100">
                             <div class="single-post post-style-1">
@@ -57,11 +34,11 @@
 
                                     <h4 class="title"><a href="{{ route('post.details',$post->slug) }}"><b>{{ $post->title }}</b></a></h4>
 
-
                                     <ul class="post-footer">
+
                                         <li>
                                             @guest
-                                                <a href="javascript:void(0);" onclick="toastr.info('You should login first to add post to your favourite.','Info',{
+                                                <a href="javascript:void(0);" onclick="toastr.info('To add favorite list. You need to login first.','Info',{
                                                         closeButton: true,
                                                         progressBar: true,
                                                     })"><i class="ion-heart"></i>{{ $post->favorite_to_users->count() }}</a>
@@ -73,26 +50,34 @@
                                                     @csrf
                                                 </form>
                                             @endguest
+
                                         </li>
-                                        <li><a href="#"><i class="ion-chatbubble"></i>6</a></li>
-                                        <li><a href="#"><i class="ion-eye"></i>{{$post->view_count}}</a></li>
+                                        <li><a href="#"><i class="ion-chatbubble"></i>{{ $post->comments->count() }}</a></li>
+                                        <li><a href="#"><i class="ion-eye"></i>{{ $post->view_count }}</a></li>
                                     </ul>
 
                                 </div><!-- blog-info -->
                             </div><!-- single-post -->
                         </div><!-- card -->
                     </div><!-- col-lg-4 col-md-6 -->
-                @endforeach
-
+                @empty
+                    <div class="col-lg-12 col-md-12">
+                        <div class="card h-100">
+                            <div class="single-post post-style-1 p-2">
+                                <strong>No Post Found :(</strong>
+                            </div><!-- single-post -->
+                        </div><!-- card -->
+                    </div><!-- col-lg-4 col-md-6 -->
+                @endforelse
             </div><!-- row -->
 
-            <a class="load-more-btn" href="#"><b>LOAD MORE</b></a>
+            {{--{{ $posts->links() }}--}}
 
         </div><!-- container -->
     </section><!-- section -->
+
 @endsection
 
 @push('js')
-    <script src="{{asset('assets/frontend/js/swiper.js')}}"></script>
 
 @endpush
