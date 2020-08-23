4b1e06bb7f85a511490cda569dc26b4329a24b4e
JoeAshraf <youssefashraf397@yahoo.com>
2020-08-23 19:31:15 +0200

    Make the api base by passbort package and make authincated controller api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', 'API\AuthController@login');
+Route::post('register', 'API\AuthController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'API\AuthController@details');
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
